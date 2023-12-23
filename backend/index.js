bb9090ddde4a771094caa83e3e252d360be9d5dc
Dhruvv <chanddhruv00@gmail.com>
2023-12-24 04:32:02 +0530

    final commit

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,9 @@
-npm i
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://Dhruv-Devtown:Devtown1234@mern-movie-booking.ufokbjt.mongodb.net/").listen(()=>app.listen(3000, () => {
+    console.log("conected to the server + port")
+})
+).catch(e=>console.log(e))
+
